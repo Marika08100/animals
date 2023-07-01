29a336bba3305b7a6ffa1156cb518f4ce020cf3a
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-07-01 11:01:47 +0300

    Dog class and Main class is rdy

---
+++ src/FileReaderHandler.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileReaderHandler {
+    public static List<String> fileReader(String filePath) {
+        List<String> strings = new ArrayList<>();
+        File file = new File(filePath);
+        try {
+            Scanner sc = new Scanner(file);
+            while ((sc.hasNextLine())) {
+                strings.add(sc.nextLine());
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("The file is not found!");
+        }
+        return strings;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = FileReaderHandler.fileReader("res/dog.txt");
+        List<Dog> dogs = new ArrayList<>();
+        for(String line : list){
+            String[] parts = line.split(",");
+            dogs.add(new Dog(parts[0],Integer.parseInt(parts[1]),parts[2]));
+        }
+
+
+    }
 }
