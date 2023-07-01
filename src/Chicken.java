17841c33f554a69af52060e12f691781381cbc46
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-07-01 11:40:22 +0300

    Chicken task is rdy

---
+++ src/Chicken.java
@@ -0,0 +1,3 @@
+public record Chicken(int eggPerSec, double weight) {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,30 @@ public class Main {
     public static void main(String[] args) {
         List<String> list = FileReaderHandler.fileReader("res/dog.txt");
         List<Dog> dogs = new ArrayList<>();
-        for(String line : list){
-            String[] parts = line.split(",");
-            dogs.add(new Dog(parts[0],Integer.parseInt(parts[1]),parts[2]));
+        List<String> list1 = FileReaderHandler.fileReader("res/chicken.txt");
+        List<Chicken> chickens = new ArrayList<>();
+        for (String line : list) {
+            dogs.add(makeDogFromLine(line, ","));
+        }
+        for (var actual : dogs) {
+            System.out.println(actual);
+        }
+        for (String line : list1) {
+            chickens.add(makeChickenFromLine(line, ","));
+        }
+        for (var actual : chickens) {
+            System.out.println(actual);
         }
 
+    }
 
+    public static Dog makeDogFromLine(String file, String regex) {
+        String[] parts = file.split(regex);
+        return new Dog(parts[0], Integer.parseInt(parts[1]), parts[2]);
+    }
+
+    public static Chicken makeChickenFromLine(String file, String regex) {
+        String[] parts = file.split(regex);
+        return new Chicken(Integer.parseInt(parts[0]),Double.parseDouble(parts[1]));
     }
 }
